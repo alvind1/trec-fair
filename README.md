5c7549224047b37eca5a68bbffddf3d0323828a4
Alvin Dai <alvin.dai.02@gmail.com>
2022-08-24 22:07:25 -0400

    centralize create t5 input

--- README.md
+++ README.md
@@ -45,3 +45,29 @@ python convert_trec_fair_reldocs_to_qrels.py \
   --run trec-fair-2022/runs/trecfair2022.train.run100000.plain_corpus_ignore_duplicates.bm25.txt \
   --output trec-fair-2022/topics-and-qrels/trecfair2022.train.qrels_w_run_negative_samples.txt
 ```
+
+# Preparing T5 input
+
+2021 Ex.
+```bash
+python create_trec_fair_monot5_input.py \
+  --corpus trec-fair-2021/collections/text/trecfair2021.text.jsonl \
+  --topics trec-fair-2021/topics-and-qrels/trecfair2021.train.queries.tsv \
+  --qrel trec-fair-2021/topics-and-qrels/trecfair2021.train.qrels.txt \
+  --output_t5_texts trec-fair-2021/t5_inputs/trecfair2021.train.t5input.text_corpus.qrels.txt \
+  --output_t5_ids trec-fair-2021/t5_inputs/trecfair2021.train.t5input.text_corpus.qrels.ids.txt \
+  --stride 4 \
+  --max_length 8
+```
+
+2022 Ex.
+```bash
+python create_trec_fair_monot5_input.py \
+  --corpus trec-fair-2022/collections/plain-ignore-duplicates/trecfair2022.plain_ignore_duplicates.jsonl \
+  --topics trec-fair-2022/topics-and-qrels/trecfair2022.train.queries.tsv \
+  --qrel trec-fair-2022/topics-and-qrels/trecfair2022.train.qrels.txt \
+  --output_t5_texts trec-fair-2022/t5_inputs/trecfair2022.train.t5input.plain_corpus_ignore_duplicates.qrels.txt \
+  --output_t5_ids trec-fair-2022/t5_inputs/trecfair2022.train.t5input.plain_corpus_ignore_duplicates.qrels.ids.txt \
+  --stride 4 \
+  --max_length 8
+```
---
+++ create_trec_fair_monot5_input.py
@@ -0,0 +1,212 @@
+import argparse
+import json
+import logging
+import re
+import spacy
+from tqdm import tqdm
+
+import config
+
+def load_corpus(path):
+    logging.info('loading corpus...')
+    corpus = {}
+    total = config.get_documents(YEAR)
+    with open(path, 'r') as f_corpus:
+        for line in tqdm(f_corpus, total=total):
+            line = json.loads(line)
+            if YEAR == '2021':
+                corpus[int(line['id'])] = {'contents': line['contents'], 'title': line['raw']['title']}
+            elif YEAR == '2022':
+                corpus[int(line['id'])] = {'contents': line['contents'], 'title': line['title']}
+            else:
+                raise ValueError('YEAR must be "2021" or "2022"')
+    return corpus
+
+
+def load_queries(path):
+    logging.info('loading queries...')
+    queries = {}
+    total = config.get_queries(YEAR, DATA_MODE)
+    with open(path, 'r') as f_queries:
+        for line in tqdm(f_queries, total=total):
+            line = line.rstrip().split('\t')
+            query_id = int(line[0])
+            query, keywords = line[1].split(maxsplit=1)
+            queries[query_id] = (query, keywords)
+    return queries
+
+
+def load_runs(path):
+    logging.info('loading runs...')
+    run = {}
+    with open(path, 'r') as f_runs:
+        for line in tqdm(f_runs):
+            query_id, _, doc_id, _, _, _ = line.split()
+            query_id = int(query_id)
+            doc_id = int(doc_id.rstrip('\n'))
+            if query_id not in run:
+                run[query_id] = []
+            run[query_id].append((doc_id, -1))
+    return run
+
+def load_qrels(path):
+    logging.info('loading qrels...')
+    run = {}
+    with open(path, 'r') as f_qrels:
+        for line in tqdm(f_qrels):
+            query_id, _, doc_id, rel = line.split()
+            query_id = int(query_id)
+            doc_id = int(doc_id)
+            rel = int(rel.rstrip('\n'))
+            if query_id not in run:
+                run[query_id] = []
+            run[query_id].append((doc_id, rel))
+    return run
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Create T5 input for Fair Trec 2021 queries.')
+    parser.add_argument('--topics', required=True,
+                        help='Topics file (queries)')
+    parser.add_argument('--run', required=False, default='',
+                        help='TREC run file')
+    parser.add_argument('--qrel', required=False, default='',
+                        help='qrels file')
+    parser.add_argument('--corpus', type=str, required=True)
+    parser.add_argument('--output_t5_texts', required=True, default='',
+                        help='output file')
+    parser.add_argument('--output_t5_ids', required=True, default='',
+                        help='output file')
+    parser.add_argument('--stride', type=int, default=5, help='')
+    parser.add_argument('--max_length', type=int, default=10, help='')
+    parser.add_argument('--only-first-segment', action='store_true')
+    parser.add_argument('--ignore-naming', action='store_true')
+    parser.add_argument('--verbose', action='store_true')
+    args = parser.parse_args()
+
+    if args.run == '' and args.qrel == '':
+        parser.error("at least one of --run, --qrel is required")
+        quit()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    DATA_MODE = ''
+    if re.search('train', args.topics, re.IGNORECASE):
+        DATA_MODE = 'TRAIN'
+        logging.info('Assuming train data mode')
+    elif re.search('eval', args.topics):
+        DATA_MODE = 'EVAL'
+        logging.info('Assuming eval data mode')
+    else:
+        raise Exception('file naming error: expected topics type to be specified by having "train" or "eval" in file name')
+
+    YEAR = ''
+    if re.search('2021', args.topics):
+        YEAR = '2021'
+        logging.info('Assuming 2021')
+    elif re.search('2022', args.topics):
+        YEAR = '2022'
+        logging.info('Assuming 2022')
+    else:
+        raise Exception(
+            'file naming error: could not deduce year as either "2021" or "2022" from topics file name')
+
+
+    if args.qrel != '':
+        assert re.search(DATA_MODE, args.qrel, re.IGNORECASE) != None
+        assert re.search(YEAR, args.qrel, re.IGNORECASE) != None
+    else:
+        assert re.search(DATA_MODE, args.run, re.IGNORECASE) != None
+        assert re.search(YEAR, args.run, re.IGNORECASE) != None
+    assert re.search(DATA_MODE, args.output_t5_texts, re.IGNORECASE) != None
+    assert re.search(YEAR, args.output_t5_texts, re.IGNORECASE) != None
+    assert re.search(DATA_MODE, args.output_t5_ids, re.IGNORECASE) != None
+    assert re.search(YEAR, args.output_t5_ids, re.IGNORECASE) != None
+
+    queries = load_queries(args.topics)
+    run = {}
+    if args.run != '':
+        runs = load_runs(args.run)
+    elif args.qrel != '':
+        runs = load_qrels(args.qrel)
+    assert len(runs) > 0
+    corpus = load_corpus(args.corpus)
+
+    logging.info('creating t5 inputs...')
+    nlp = spacy.blank("en")
+    nlp.add_pipe("sentencizer")
+    n_docs = 0
+    n_segments = 0
+    n_no_segments = 0
+    n_no_content = 0
+    n_not_found = 0
+    set_ids = set()
+    with open(args.output_t5_texts, 'w') as fout_t5_texts,  \
+            open(args.output_t5_ids, 'w') as fout_t5_ids:
+        for (query_id, docs) in tqdm(runs.items()):
+            query, keywords = queries[query_id]
+            for doc_id, rel in docs:
+                if doc_id not in corpus:
+                    #print(f'Doc id not found: {doc_id}')
+                    n_not_found += 1
+                    continue
+                contents = corpus[doc_id]
+                n_docs += 1
+                passage_text = contents['contents']
+                doc_title = contents['title']
+
+                # Remove any duplicated spaces or line breaks.
+                passage_text = ' '.join(passage_text.split())
+
+                doc = nlp(passage_text)
+                sentences = [sent.text.strip() for sent in doc.sents]
+                doc_title = ' '.join(doc_title.split())
+                if not sentences:
+                    n_no_segments += 1
+                    sentences = [""]
+                for i in range(0, len(sentences), args.stride):
+                    segment = ' '.join(
+                        sentences[i:i + args.max_length]).strip()
+                    if doc_title:
+                        if doc_title.startswith('.'):
+                            doc_title = doc_title[1:]
+
+                        if i == 0:
+                            segment = '. '.join([doc_title, segment[len(doc_title):]])
+                        else:
+                            segment = '. '.join([doc_title, segment])
+
+                    # Remove starting #'s as T5 skips those lines by default.
+                    segment = re.sub(r'^#*', '', segment)
+
+                    fout_t5_ids.write(f'{query_id}\t{doc_id}\t{i}\n')
+
+                    if args.qrel != '':
+                        relevant = ''
+                        if rel == 1:
+                            relevant = '\ttrue'
+                        elif rel == 0:
+                            relevant = '\tfalse'
+                        assert relevant != ''
+                        if keywords == "":
+                            fout_t5_texts.write(
+                                f'Query: {query} Document: {segment} Relevant:{relevant}\n')
+                        else:
+                            fout_t5_texts.write(
+                                f'Query: {query} Keywords: {keywords} Document: {segment} Relevant:{relevant}\n')
+                    n_segments += 1
+                    if i + args.max_length >= len(sentences):
+                        break
+                    if args.only_first_segment:
+                        break
+
+    logging.info(f'{n_no_content} examples with only title')
+    logging.info(f'{n_not_found} examples not found')
+    logging.info(f'Wrote {n_segments} segments from {n_docs} docs.')
+    logging.info(f'There were {n_no_segments} docs without segments/sentences.')
+    logging.info('Done')
